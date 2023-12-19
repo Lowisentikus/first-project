abc2948eb99c862749a2728ab9b2ed0dd7a3a7a7
Serhii Lomovtsev <mr.lomovtsev00@gmail.com>
2023-12-19 23:47:08 +0200

    Refactor sum function to sub function

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ def print_hi(name):
     print(f'Hi, {name}')
 
 
-def sum(number1: int, number2: int):
-    return number1 + number2
+def sub(number1: int, number2: int):
+    return number1 - number2
 
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-    sum(2, 2)
+    sum(4, 8)
 
