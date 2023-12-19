e708d2d41e744042b03107c154886c70de72c80b
Serhii Lomovtsev <mr.lomovtsev00@gmail.com>
2023-12-19 23:41:52 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+def sum(number1: int, number2: int):
+    return number1 + number2
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    sum(2, 2)
+
